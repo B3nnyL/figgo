e4d2349478eefdd9495ca66cd8c057b29eeff103
b3nnyL <shuyinglin47@gmail.com>
2019-01-30 12:27:33 +1100

    [feat] color

---
+++ lib/models/color.ts
@@ -0,0 +1,42 @@
+import { rgbaToString, rgbToHex } from "../helper";
+export default class Color {
+  private name: string;
+  private rFloat: number;
+  private gFloat: number;
+  private bFloat: number;
+  private aFloat: number;
+  constructor(name: string, r: number, g: number, b: number, a: number) {
+    this.name = name.replace(/\s+/g, "-").toUpperCase();
+    this.rFloat = r;
+    this.gFloat = g;
+    this.bFloat = b;
+    this.aFloat = a;
+  }
+
+  get formatedName() {
+    return this.name;
+  }
+  get HEX(): string {
+    if (this.rgbaIsValid()) {
+      return rgbToHex(this.rFloat, this.gFloat, this.bFloat);
+    }
+  }
+
+  get RGBA(): string {
+    if (this.rgbaIsValid()) {
+      return rgbaToString(this.rFloat, this.gFloat, this.bFloat, this.aFloat);
+    }
+  }
+
+  get CssColor(): string {
+    const color = this.aFloat < 1 ? this.RGBA : this.HEX;
+    return `$${this.name}:${color}`;
+  }
+
+  private rgbaIsValid(): boolean {
+    if (!(this.rFloat || this.gFloat || this.bFloat || this.aFloat)) {
+      return false;
+    }
+    return true;
+  }
+}
---
+++ test/color.test.ts
@@ -0,0 +1,32 @@
+import Color from "../lib/models/color";
+const fakeColor = {
+  name: "fake-color",
+  r: 0.8549019607843137,
+  g: 0.6470588235294118,
+  b: 0,
+  a: 1
+};
+
+describe("color class ", () => {
+  const color = new Color(
+    fakeColor.name,
+    fakeColor.r,
+    fakeColor.g,
+    fakeColor.b,
+    fakeColor.a
+  );
+  test("should set name with format", () => {
+    expect(color.formatedName).toBe("FAKE-COLOR");
+  });
+  test("should get HEX ", () => {
+    expect(color.HEX).toBe("#daa500");
+  });
+
+  test("should get rgba in string", () => {
+    expect(color.RGBA).toBe("rgba(218,165,0,1.00)");
+  });
+
+  test("should return css string", () => {
+    expect(color.CssColor).toBe("$FAKE-COLOR:#daa500");
+  });
+});
