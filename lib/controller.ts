c5702344a96c0db324647d8e4e9ee655fb7c6571
b3nnyL <shuyinglin47@gmail.com>
2019-01-30 16:44:12 +1100

    [feat] store content to scss file according to output directory

--- lib/controller.ts
+++ lib/controller.ts
@@ -1,13 +1,17 @@
 import * as fs from "fs";
+import { join } from "path";
 
-export function saveColor(colors: string[]) {
-  fs.writeFileSync("./myColorToken.scss", colors.join("\n"), "utf-8");
+export function saveColor(outDir: string, colors: string[]) {
+  const dist = join(outDir, "_colorToken.scss");
+  fs.writeFileSync(dist, colors.join("\n"), "utf-8");
 }
 
-export function saveSpaces(spaces: string[]) {
-  fs.writeFileSync("./mySpacesToken.scss", spaces.join("\n"), "utf-8");
+export function saveSpaces(outDir: string, spaces: string[]) {
+  const dist = join(outDir, "_spaceToken.scss");
+  fs.writeFileSync(dist, spaces.join("\n"), "utf-8");
 }
 
-export function saveTypos(typos: string[]) {
-  fs.writeFileSync("./myTypographyToken.scss", typos.join("\n"), "utf-8");
+export function saveTypos(outDir: string, typos: string[]) {
+  const dist = join(outDir, "_typographicToken.scss");
+  fs.writeFileSync(dist, typos.join("\n"), "utf-8");
 }
