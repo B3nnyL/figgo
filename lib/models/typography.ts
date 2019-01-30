ffc42b6c5bc9434306ecc318ed4cbf76e795e0ee
b3nnyL <shuyinglin47@gmail.com>
2019-01-30 13:33:54 +1100

    [feat] typography

---
+++ lib/models/typography.ts
@@ -0,0 +1,46 @@
+export default class Typography {
+  private name: string;
+  private fontFamily: string;
+  private fontWeight: number;
+  private fontSize: number;
+  private letterSpacing: number;
+  private lineHeight: number;
+  constructor(
+    name: string,
+    fFamily: string,
+    fWeight: number,
+    fSize: number,
+    lSpacing: number,
+    lHeight: number
+  ) {
+    this.name = name.replace(/\s+/g, "-").toUpperCase();
+    this.fontFamily = fFamily;
+    this.fontWeight = fWeight;
+    this.fontSize = fSize;
+    this.letterSpacing = lSpacing;
+    this.lineHeight = lHeight;
+  }
+  get formatedName(): string {
+    return this.name;
+  }
+  get CSSFontFamily(): string {
+    const handler = `${this.name}-FONT-FAMILY`;
+    return `$${handler}: ${this.fontFamily}`;
+  }
+  get CSSFontWeight(): string {
+    const handler = `${this.name}-FONT-WEIGHT`;
+    return `$${handler}: ${this.fontWeight}`;
+  }
+  get CSSFontSize(): string {
+    const handler = `${this.name}-FONT-SIZE`;
+    return `$${handler}: ${this.fontSize}px`;
+  }
+  get CSSLetterSpacing(): string {
+    const handler = `${this.name}-LETTER-SPACING`;
+    return `$${handler}: ${this.letterSpacing}px`;
+  }
+  get CSSLineHeight(): string {
+    const handler = `${this.name}-LINE-HEIGHT`;
+    return `$${handler}: ${this.lineHeight}px`;
+  }
+}
---
+++ test/typography.test.ts
@@ -0,0 +1,45 @@
+import Typography from "../lib/models/typography";
+const typography = {
+  fontFamily: "Helvetica Neue",
+  fontSize: 36,
+  fontWeight: 700,
+  letterSpacing: 0.5,
+  lineHeight: 54,
+  name: "fake-typo"
+};
+
+describe("typography", () => {
+  const fakeTypo = new Typography(
+    typography.name,
+    typography.fontFamily,
+    typography.fontWeight,
+    typography.fontSize,
+    typography.letterSpacing,
+    typography.lineHeight
+  );
+  test("should return formated name", () => {
+    expect(fakeTypo.formatedName).toBe("FAKE-TYPO");
+  });
+
+  test("should return CSS formated font family", () => {
+    expect(fakeTypo.CSSFontFamily).toBe(
+      "$FAKE-TYPO-FONT-FAMILY: Helvetica Neue"
+    );
+  });
+
+  test("should return CSS formated font weight", () => {
+    expect(fakeTypo.CSSFontWeight).toBe("$FAKE-TYPO-FONT-WEIGHT: 700");
+  });
+
+  test("should return CSS formated font size", () => {
+    expect(fakeTypo.CSSFontSize).toBe("$FAKE-TYPO-FONT-SIZE: 36px");
+  });
+
+  test("should return CSS formated font letter spacing", () => {
+    expect(fakeTypo.CSSLetterSpacing).toBe("$FAKE-TYPO-LETTER-SPACING: 0.5px");
+  });
+
+  test("should return CSS formated font line height", () => {
+    expect(fakeTypo.CSSLineHeight).toBe("$FAKE-TYPO-LINE-HEIGHT: 54px");
+  });
+});
