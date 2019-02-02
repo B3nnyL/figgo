c408daa141805d7bb9898d5bb7751379829073cb
b3nnyL <shuyinglin47@gmail.com>
2019-02-02 23:15:16 +1100

    [feat] usage hint

---
+++ lib/hint.ts
@@ -0,0 +1,20 @@
+export const hints = `
+  How to use
+    $ figgo [<options> ...]
+    Options
+      --init, -i         Setup figma board
+      --sync, -s         Sync tokens
+      --list, -l         List boards
+      --remove, -r       Remove board
+      --help, -h         Show help message
+      --version, -v      Show installed version
+    Examples
+      $ figgo --init
+      $ figgo --init board_name board_id output_directory output_format
+      $ figgo --list
+      $ figgo --sync
+      $ figgo --sync board_name
+      $ figgo --remove board_name
+      $ figgo --h
+      $ figgo --v
+`;
