96dfb7476a50c117c4ecfcd6a41acdfd49ac895c
b3nnyL <shuyinglin47@gmail.com>
2019-02-03 14:43:40 +1100

    [test] service get content from figma

--- lib/service.ts
+++ lib/service.ts
@@ -1,13 +1,15 @@
 import fetch from "node-fetch";
 import INAccount from "./interfaces/account.interface";
 import IBoard from "./interfaces/board.interface";
+import Color from "./models/color";
+import Space from "./models/spacing";
+import Typography from "./models/typography";
 
 export async function getAccount(token: string): Promise<INAccount> {
   const url = `https://api.figma.com/v1/me`;
   try {
     const result = await fetch(url, {
       headers: { "X-FIGMA-TOKEN": token },
-      // tslint:disable-next-line:trailing-comma
       method: "get"
     });
     return result.json();
@@ -27,4 +29,112 @@ export async function getBoard(token: string, board: string): Promise<IBoard> {
   } catch (err) {
     console.log(err);
   }
+}
+
+export async function getColors(
+  token: string,
+  board: string
+): Promise<string[]> {
+  const url = `https://api.figma.com/v1/files/${board}`;
+  let result;
+  try {
+    result = await fetch(url, {
+      headers: { "X-FIGMA-TOKEN": token },
+      method: "get"
+    });
+  } catch (e) {
+    console.log(e.json());
+  }
+
+  const data = await result.json();
+  const frames = data.document.children[0].children;
+  const array = [];
+  const colorFrame = frames.filter(frame => frame.name === "Palette");
+  const colorBlocks = colorFrame[0].children;
+  for (const i in colorBlocks) {
+    if (colorBlocks[i].fills[0].type === "SOLID") {
+      const name = colorBlocks[i].name;
+      const rgba = colorBlocks[i].fills[0].color;
+      const newColor = new Color(name, rgba.r, rgba.g, rgba.b, rgba.a);
+      array.push(newColor.CssColor);
+    }
+  }
+  console.log(array);
+  return array;
+}
+
+export async function getSpaces(
+  token: string,
+  board: string
+): Promise<string[]> {
+  const url = `https://api.figma.com/v1/files/${board}`;
+  const result = await fetch(url, {
+    headers: { "X-FIGMA-TOKEN": token },
+    method: "get"
+  });
+
+  const data = await result.json();
+  const frames = data.document.children[0].children;
+  const array = [];
+  const spaceFrame = frames.filter(frame => frame.name === "Spaces");
+  const spaceBlocks = spaceFrame[0].children;
+  for (const i in spaceBlocks) {
+    if (spaceBlocks) {
+      const name = spaceBlocks[i].name;
+      const value = spaceBlocks[i].children[0].absoluteBoundingBox.width;
+      const newSpace = new Space(name, value);
+      array.push(newSpace.cssValue);
+    }
+  }
+  console.log(array);
+  return array;
+}
+
+export async function getTypographics(
+  token: string,
+  board: string
+): Promise<string[]> {
+  const url = `https://api.figma.com/v1/files/${board}`;
+  const result = await fetch(url, {
+    headers: { "X-FIGMA-TOKEN": token },
+    method: "get"
+  });
+
+  const data = await result.json();
+  const frames = data.document.children[0].children;
+  const array = [];
+  const typoFrame = frames.filter(frame => frame.name === "Typographic");
+  const typoBlocks = typoFrame[0].children.filter(
+    blocks => blocks.type === "TEXT"
+  );
+  for (const i in typoBlocks) {
+    if (typoBlocks[i]) {
+      const name = typoBlocks[i].name;
+      const style = typoBlocks[i].style;
+      const {
+        fontFamily,
+        fontWeight,
+        fontSize,
+        letterSpacing,
+        lineHeightPx
+      } = style;
+      const newTypo = new Typography(
+        name,
+        fontFamily,
+        fontWeight,
+        fontSize,
+        letterSpacing,
+        lineHeightPx
+      );
+      array.push(
+        newTypo.CSSFontFamily,
+        newTypo.CSSFontSize,
+        newTypo.CSSFontWeight,
+        newTypo.CSSLetterSpacing,
+        newTypo.CSSLineHeight
+      );
+    }
+  }
+  console.log(array);
+  return array;
 }
--- test/service.test.ts
+++ test/service.test.ts
@@ -1,5 +1,5 @@
 import { TOKEN } from "../lib/envConfig";
-import { getAccount, getBoard } from "../lib/service";
+import { getAccount, getBoard, getColors } from "../lib/service";
 
 describe("service can ", () => {
   test("get user info", () => {
@@ -12,7 +12,15 @@ describe("service can ", () => {
   test("get board", () => {
     getBoard(TOKEN, "gFmmbJ0648oZLD2Y0arRLuy2").then(res => {
       const result = res.name;
+      console.log(result);
       expect(result).toBe("Lustigt");
+    });
+  });
+
+  test("get colors", () => {
+    getColors(TOKEN, "gFmmbJ0648oZLD2Y0arRLuy2").then(res => {
+      const result = res;
+      expect(res).toBe("23");
     });
   });
 });
