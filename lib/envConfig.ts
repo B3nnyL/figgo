a59277c22ed15ff65cde204aa76d9d0f2bd07611
b3nnyL <shuyinglin47@gmail.com>
2019-02-12 19:30:43 +1100

    [refactor] env config

--- lib/envConfig.ts
+++ lib/envConfig.ts
@@ -2,21 +2,17 @@ import * as dotenv from "dotenv";
 
 let token;
 let board;
-let dir;
 dotenv.config();
 switch (process.env.NODE_ENV) {
   case "test":
     token = process.env.DEV_TOKEN;
     board = process.env.DEV_BOARD;
-    dir = process.env.TEST_DIR;
     break;
   case "dev":
     token = process.env.DEV_TOKEN;
     board = process.env.DEV_BOARD;
-    dir = process.env.DEV_DIR;
     break;
 }
 
 export const TOKEN = token;
 export const BOARD = board;
-export const DIR = dir;
