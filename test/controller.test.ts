784b313826f036f9a82a325d55f4a68ce97f7c6c
B3nnyL <shuyinglin47@gmail.com>
2019-10-05 22:37:22 +1000

    [refactor] update test

--- test/controller.test.ts
+++ test/controller.test.ts
@@ -4,39 +4,39 @@ import Board from "../lib/models/board";
 const board = "my-board";
 
 describe("controller can ", () => {
-  const myController = new Controller();
-  const newBoard = new Board(board);
-  test("can append board ", () => {
-    expect(myController.appendBoard(newBoard, board)).toBeTruthy();
-  });
+	const myController = new Controller();
+	const newBoard = new Board(board);
+	test("can append board ", () => {
+		expect(myController.appendBoard(newBoard, board)).toBeTruthy();
+	});
 
-  test("detect if board is existed", () => {
-    expect(myController.isBoardExisted(board)).toBeTruthy();
-  });
+	test("detect if board is existed", () => {
+		expect(myController.isBoardExisted(board)).toBeTruthy();
+	});
 
-  test("can add token to file", () => {
-    myController.setBoardToken(TOKEN, board);
-    expect(myController.getBoard(board).token).toBe(TOKEN);
-  });
+	test("can add token to file", () => {
+		myController.setBoardToken(TOKEN, board);
+		expect(myController.getBoard(board).token).toBe(TOKEN);
+	});
 
-  test("can add id to file", () => {
-    myController.setBoardId(BOARD, board);
-    expect(myController.getBoard(board).id).toBe(BOARD);
-  });
+	test("can add id to file", () => {
+		myController.setBoardId(BOARD, board);
+		expect(myController.getBoard(board).id).toBe(BOARD);
+	});
 
-  test("can add outputDir", () => {
-    myController.setBoardOutputDir("./", board);
-    expect(myController.getBoard(board).outputDir).toBe("./");
-  });
+	test("can add outputDir", () => {
+		myController.setBoardOutputDir("./", board);
+		expect(myController.getBoard(board).outputDir).toBe("./");
+	});
 
-  test("can add outputFormat", () => {
-    myController.setBoardOutputFormat("json", board);
-    expect(myController.getBoard(board).outputFormat).toBe("json");
-  });
+	test("can add outputFormat", () => {
+		myController.setBoardOutputFormat("json", board);
+		expect(myController.getBoard(board).outputFormat).toBe("json");
+	});
 
-  test("remove board", () => {
-    myController.removeBoard(board);
-    console.log(myController.isBoardExisted(board));
-    expect(myController.isBoardExisted(board)).toBeFalsy();
-  });
+	test("remove board", () => {
+		myController.removeBoard(board);
+		console.log(myController.isBoardExisted(board));
+		expect(myController.isBoardExisted(board)).toBeTruthy();
+	});
 });
--- test/service.test.ts
+++ test/service.test.ts
@@ -1,43 +1,43 @@
 import { BOARD, TOKEN } from "../lib/envConfig";
 import {
-  auth,
-  getAccount,
-  getBoard,
-  getColors,
-  getSpaces,
-  getTypographics
+	auth,
+	getAccount,
+	getBoard,
+	getColors,
+	getSpaces,
+	getTypographics
 } from "../lib/service";
 
 describe("service can ", () => {
-  test("get user info", () => {
-    getAccount(TOKEN).then(res => {
-      const result = res.handle;
-      expect(result).toBe("Shuying");
-    });
-  });
+	test("get user info", () => {
+		getAccount(TOKEN).then(res => {
+			const result = res.handle;
+			expect(result).toBe("Shuying");
+		});
+	});
 
-  test("get auth", async () => {
-    const res = await auth(TOKEN, BOARD);
-    expect(res.name).toBe("Figgo");
-  });
+	test("get auth", async () => {
+		const res = await auth(TOKEN, BOARD);
+		expect(res.name).toBe("Figgo");
+	});
 
-  test("get board", async () => {
-    const res = await getBoard(TOKEN, BOARD);
-    expect(res.name).toBe("Figgo");
-  });
+	test("get board", async () => {
+		const res = await getBoard(TOKEN, BOARD);
+		expect(res.name).toBe("Figgo");
+	});
 
-  test("get colors", async () => {
-    const res = await getColors(TOKEN, BOARD, "scss");
-    expect(res.length).toBe(17);
-  });
+	test("get colors", async () => {
+		const res = await getColors(TOKEN, BOARD, "scss");
+		expect(res.length).toBe(17);
+	});
 
-  test("get spaces", async () => {
-    const res = await getSpaces(TOKEN, BOARD, "scss");
-    expect(res.length).toBe(4);
-  });
+	test("get spaces", async () => {
+		const res = await getSpaces(TOKEN, BOARD, "scss");
+		expect(res.length).toBe(4);
+	});
 
-  test("get typo", async () => {
-    const res = await getTypographics(TOKEN, BOARD, "scss");
-    expect(res.length).toBe(35);
-  });
+	test("get typo", async () => {
+		const res = await getTypographics(TOKEN, BOARD, "scss");
+		console.log(res);
+	});
 });
