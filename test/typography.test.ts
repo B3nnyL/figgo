f5286adee03328fc4d434e45618c544b41168f5a
B3nnyL <shuyinglin47@gmail.com>
2019-10-05 22:12:02 +1000

    [new] refactor typograph and add css variable support

--- test/typography.test.ts
+++ test/typography.test.ts
@@ -1,47 +1,52 @@
 import Typography from "../lib/models/typography";
 const typography = {
-  fontFamily: "Helvetica Neue",
-  fontSize: 36,
-  fontWeight: 700,
-  letterSpacing: 0.5,
-  lineHeight: 54,
-  name: "fake-typo"
+	fontFamily: "Helvetica Neue",
+	fontSize: 36,
+	fontWeight: 700,
+	letterSpacing: 0.5,
+	lineHeight: 54,
+	name: "fake-typo"
 };
 
 describe("typography", () => {
-  const fakeTypo = new Typography(
-    typography.name,
-    typography.fontFamily,
-    typography.fontWeight,
-    typography.fontSize,
-    typography.letterSpacing,
-    typography.lineHeight
-  );
-  test("should return formated name", () => {
-    expect(fakeTypo.formatedName).toBe("FAKE-TYPO");
-  });
+	const fakeTypo = new Typography(
+		typography.name,
+		typography.fontFamily,
+		typography.fontWeight,
+		typography.fontSize,
+		typography.letterSpacing,
+		typography.lineHeight
+	);
 
-  test("should return CSS formated font family", () => {
-    expect(fakeTypo.CSSFontFamily.trim()).toBe(
-      "$FAKE-TYPO-FONT-FAMILY: 'Helvetica Neue',-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;".trim()
-    );
-  });
+	test("should return SCSS formated font family", () => {
+		expect(fakeTypo.SCSSFontFamily.trim()).toBe(
+			"$FAKE-TYPO-FONT-FAMILY: 'Helvetica Neue',-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;".trim()
+		);
+	});
 
-  test("should return CSS formated font weight", () => {
-    expect(fakeTypo.CSSFontWeight).toBe("$FAKE-TYPO-FONT-WEIGHT: 700;");
-  });
+	test("should return SCSS formated font weight", () => {
+		expect(fakeTypo.SCSSFontWeight).toBe("$FAKE-TYPO-FONT-WEIGHT: 700;");
+	});
 
-  test("should return CSS formated font size", () => {
-    expect(fakeTypo.CSSFontSize).toBe("$FAKE-TYPO-FONT-SIZE: 36.00px;");
-  });
+	test("should return SCSS formated font size", () => {
+		expect(fakeTypo.SCSSFontSize).toBe("$FAKE-TYPO-FONT-SIZE: 36.00px;");
+	});
 
-  test("should return CSS formated font letter spacing", () => {
-    expect(fakeTypo.CSSLetterSpacing).toBe(
-      "$FAKE-TYPO-LETTER-SPACING: 0.50px;"
-    );
-  });
+	test("should return SCSS formated font letter spacing", () => {
+		expect(fakeTypo.SCSSLetterSpacing).toBe(
+			"$FAKE-TYPO-LETTER-SPACING: 0.50px;"
+		);
+	});
 
-  test("should return CSS formated font line height", () => {
-    expect(fakeTypo.CSSLineHeight).toBe("$FAKE-TYPO-LINE-HEIGHT: 54.00px;");
-  });
+	test("should return SCSS formated font line height", () => {
+		expect(fakeTypo.SCSSLineHeight).toBe("$FAKE-TYPO-LINE-HEIGHT: 54.00px;");
+	});
+
+	test("should return css variable", () => {
+		expect(fakeTypo.css).toContain("--fake-typo");
+	});
+
+	test("should return js variable", () => {
+		expect(fakeTypo.js).toContain("FAKE_TYPO");
+	});
 });
