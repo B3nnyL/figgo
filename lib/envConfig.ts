36fffa17220b149144c71dd816c9981cbcf745ce
B3nnyL <shuyinglin47@gmail.com>
2020-05-03 16:24:24 +1000

    [workflow] update packages and refactor environment variable setting

--- lib/envConfig.ts
+++ lib/envConfig.ts
@@ -1,20 +1,16 @@
-import * as dotenv from "dotenv";
 
 let token;
 let board;
 let ownerName;
-dotenv.config();
-switch (process.env.NODE_ENV) {
-	case "test":
+
+if (process.env.NODE_ENV == "test") {
 		token = process.env.TEST_TOKEN;
 		board = process.env.TEST_BOARD;
 		ownerName = process.env.TEST_OWNER;
-		break;
-	case "dev":
+} else {
 		token = process.env.DEV_TOKEN;
 		board = process.env.DEV_BOARD;
 		ownerName = process.env.DEV_OWNER;
-		break;
 }
 
 export const TOKEN = token;
